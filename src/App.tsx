d04484e4fac74bd8ece67be37a8fa4a4ba849c15
Cameron Irmas <cameron@4mile.io>
2019-11-18 16:28:41 -0800

    Update package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  DualRing,
+  Circle,
+  Default,
+  Ellipsis,
+  Facebook,
+  Grid,
+  Heart,
+  Hourglass,
+  Ring,
+  Ripple,
+  Roller,
+  Spinner,
+} from 'loading';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DualRing />
+        <Circle />
+        <Default />
+        <Ellipsis />
+        <Facebook />
+        <Grid />
+        <Heart />
+        <Hourglass />
+        <Ring />
+        <Ripple />
+        <Roller />
+        <Spinner />
       </header>
     </div>
   );
-}
+};
 
 export default App;
